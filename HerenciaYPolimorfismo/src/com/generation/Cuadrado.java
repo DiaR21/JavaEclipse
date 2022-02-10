589d3a84453f8a3e6cae31deb6154ccfe5681858
Diana Rangel <diana.randi1@gmail.com>
2022-02-09 19:42:16 -0600

    Herencia y polimorfismo

---
+++ HerenciaYPolimorfismo/src/com/generation/Cuadrado.java
@@ -0,0 +1,17 @@
+package com.generation;
+
+public class Cuadrado extends FiguraGeometrica {
+	public Cuadrado() {
+		super("Cuadrado");
+	}
+
+	@Override
+	protected float calcularArea() {
+		return this.alto*this.ancho;
+	}
+
+	@Override
+	protected float calcularPerimetro() {
+		return (2*this.alto + 2*this.ancho);
+	}
+}
---
+++ HerenciaYPolimorfismo/src/com/generation/FiguraGeometrica.java
@@ -0,0 +1,38 @@
+package com.generation;
+
+public abstract class FiguraGeometrica {	/*La clase abstracta sirve para ser una superclase y heredar atributos y metodos, pero no puede crearse objetos de ella*/
+	protected String nombre;
+	protected float alto;
+	protected float ancho;
+	
+	public FiguraGeometrica(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	protected abstract float calcularArea();	/*Método abstracto, existe pero no tiene funcionalidad hasta que se defina por las subclases*/
+	protected abstract float calcularPerimetro();
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public float getAlto() {
+		return this.alto;
+	}
+
+	public void setAlto(float alto) {
+		this.alto = alto;
+	}
+
+	public float getAncho() {
+		return this.ancho;
+	}
+
+	public void setAncho(float ancho) {
+		this.ancho = ancho;
+	}
+}
